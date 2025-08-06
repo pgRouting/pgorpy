4b48fd208aea95afbaec726917e42a9412e68b5d
cvvergara <vicky@erosion.dev>
2025-08-06 15:55:12 -0600

    (sampledata) Adding sample data for ortools

--- tools/testers/sampledata.sql
+++ tools/testers/sampledata.sql
@@ -2,6 +2,10 @@
 -- Creative Commons Attribution-Share Alike 3.0
 -- License : https://creativecommons.org/licenses/by-sa/3.0/
 
+DROP TABLE IF EXISTS bin_packing;
+DROP TABLE IF EXISTS knapsack;
+DROP TABLE IF EXISTS multiple_knapsack;
+
 -- activate_python_start
 CREATE OR REPLACE PROCEDURE activate_python_venv(venv text)
 LANGUAGE plpython3u AS
@@ -18,3 +22,34 @@ $BODY$
 $BODY$;
 
 -- activate_python_end
+
+CREATE TABLE bin_packing(
+  id SERIAL,
+  weight INTEGER);
+
+INSERT INTO bin_packing(weight)
+VALUES
+(48), (30), (19), (36), (36), (27), (42), (42), (36), (24), (30);
+
+
+CREATE TABLE knapsack(
+  id SERIAL,
+  weight INTEGER,
+  cost INTEGER);
+
+INSERT INTO knapsack(weight, cost)
+VALUES
+(12, 4), (2, 2), (1, 1), (4, 10), (1, 2);
+
+CREATE TABLE multiple_knapsack(
+  id SERIAL,
+  weight INTEGER,
+  cost INTEGER);
+
+INSERT INTO multiple_knapsack(weight, cost)
+VALUES
+(48, 10), (30, 30), (42, 25),
+(36, 50), (36, 35), (48, 30),
+(42, 15), (42, 40), (36, 30),
+(24, 35), (30, 45), (30, 10),
+(42, 20), (36, 30), (36, 25);
